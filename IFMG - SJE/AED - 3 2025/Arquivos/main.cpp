f62bae5849f5509d9b8ba15409270b35d834c963
Valdir de Souza Carvalho Neto <0082934@IFMG.EDU.BR>
2025-09-30 14:16:57 -0300

    Operação com Arquivos

--- IFMG - SJE/AED - 3 2025/Arquivos/main.cpp
+++ IFMG - SJE/AED - 3 2025/Arquivos/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include <fstream>
+#include <conio.h>
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <string>
 
 using namespace std;
 
@@ -17,7 +22,54 @@ int main() {
     //arq.flush();
     arq.close();
     */
+    /*
+    ifstream arq;
+    arq.open("arquivo.txt", ios::app);
+    if(!arq.is_open()){
+        cout << "Erro ao abrir o arquivo!" << endl;
+        return 1;
+    }
 
+    char linha[256];
+    // string linha;
+    char letra;
+
+    while(!arq.eof()){
+        arq.getline(linha, 256);
+        // getline(arq, linha);
+        arq.get(letra);
+        arq.ignore();
+    
+        cout << "Linha: " << linha << endl;
+        cout << "Caractere: " << letra << endl;
+    }
+    */
+
+    int tmemoria, fitas, i;
+    char c;
+    vector<char> mem;
+    fstream arq("dados.in", ios::in);
+    arq >> tmemoria >> fitas;
+    arq.ignore();
+    cout << "Fitas: " << fitas << endl;
+    cout << "T. Memoria: " << tmemoria << endl;
+
+
+    while(!arq.eof()){
+        i = 0;
+        while (i < tmemoria && !arq.eof())
+        {
+            arq.get(c);
+            mem.push_back(c);
+            i++;
+            arq >> ws;
+        }
+        sort(mem.begin(), mem.end());
+        cout << string(mem.begin(), mem.end()) << endl;
+        mem.clear();
+    }
+
+    arq.close();
     
     
     return 0;
