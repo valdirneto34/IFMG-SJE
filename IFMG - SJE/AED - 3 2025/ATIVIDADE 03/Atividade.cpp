6d525177228ad1c25960ca8fa4b2b1a2829c6d37
Valdir de Souza Carvalho Neto <valdirneto100c@gmail.com>
2025-10-01 21:16:40 -0300

    Atvd Arquivos

--- IFMG - SJE/AED - 3 2025/ATIVIDADE 03/Atividade.cpp
+++ IFMG - SJE/AED - 3 2025/ATIVIDADE 03/Atividade.cpp
@@ -5,6 +5,7 @@ quais problemas teve e como os superou, colocando prints do código e do resulta
 */
 
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <fstream>
 #include <algorithm>
@@ -19,48 +20,79 @@ struct Pessoa
     int idade;
 };
 
-Pessoa cadastraPessoa()
+fstream arq;
+
+int cadastraPessoa()
 {
+    arq.open("cadastros.txt", ios::app);
+    if (!arq.is_open())
+    {
+        cout << "Erro ao abrir o arquivo!" << endl;
+        return 1;
+    }
     Pessoa p;
     string altura_str;
     char opc;
-    while (true)
+
+    cout << "\nDigite o nome da pessoa: ";
+    getline(cin, p.nome);
+    cout << "Digite a altura da pessoa em metros: ";
+    cin >> altura_str;
+    cout << "Digite a idade da pessoa: ";
+    cin >> p.idade;
+
+    replace(altura_str.begin(), altura_str.end(), ',', '.');
+    p.altura = stof(altura_str);
+    cout << "\nNome..: " << p.nome << endl;
+    cout << "Altura: " << setprecision(2) << fixed << p.altura << " metros" << endl;
+    cout << "Idade.: " << p.idade << " anos\n\n";
+
+    while (opc != 'S' && opc != 'N')
     {
-        cout << "\nDigite o nome da pessoa: ";
-        getline(cin, p.nome);
-        cout << "Digite a altura da pessoa em metros: ";
-        cin >> altura_str;
-        cout << "Digite a idade da pessoa: ";
-        cin >> p.idade;
-
-        replace(altura_str.begin(), altura_str.end(), ',', '.');
-        p.altura = stof(altura_str);
-        cout << "\nNome: " << p.nome << endl;
-        cout << "Altura: " << p.altura << " metros" << endl;
-        cout << "Idade: " << p.idade << " anos\n\n";
-
-        while (true)
+        cout << "Deseja cadastrar essa pessoa? [S/N]:  ";
+        cin >> opc;
+        opc = toupper(opc);
+        if (opc != 'S' && opc != 'N')
         {
-            cout << "Deseja cadastrar essa pessoa? [S/N]:  ";
-            cin >> opc;
-            opc = toupper(opc);
-            if (opc != 'S' && opc != 'N')
-            {
-                cout << "\nOpção inválida!";
-                continue;
-            }
-            else if (opc == 'S')
-            {
-                cout << "\nPessoa cadastrada com sucesso!";
-                return p;
-            }
-            else
-            {
-                cin.ignore();
-                break;
-            }
+            cout << "\nOpção inválida! ";
         }
     }
+    if (opc == 'S')
+    {
+        arq << p.nome << endl;
+        arq << setprecision(2) << fixed << p.altura << endl;
+        arq << p.idade << endl;
+        cout << "\nPessoa cadastrada com sucesso!\n\n";
+    }
+    else
+    {
+        cout << "\nA pessoa não foi cadastrada!\n\n";
+        cin.ignore();
+    }
+    arq.close();
+}
+
+int listaPessoas()
+{
+    string nome, altura_str, idade_str;
+    arq.open("cadastros.txt", ios::in);
+    if (!arq.is_open())
+    {
+        cout << "Erro ao abrir o arquivo!" << endl;
+        return 1;
+    }
+    cout << "\n--------- PESSOAS CADASTRADAS ---------" << endl;
+    while (getline(arq, nome))
+    {
+        getline(arq, altura_str);
+        getline(arq, idade_str);
+        cout << "Nome..: " << nome << endl;
+        cout << "Altura: " << altura_str << endl;
+        cout << "Idade.: " << idade_str << endl;
+        cout << "---------------------------------------" << endl;
+    }
+
+    arq.close();
 }
 
 int main()
@@ -91,41 +123,12 @@ int main()
         }
         case 1:
         {
-            pessoa = cadastraPessoa();
-            fstream arq("arquivo.txt", ios::app);
-            if (!arq.is_open())
-            {
-                cout << "Erro ao abrir o arquivo!" << endl;
-                return 1;
-            }
-
-            arq << pessoa.nome << endl;
-            arq << pessoa.altura << endl;
-            arq << pessoa.idade << endl;
-
-            arq.close();
+            cadastraPessoa();
             break;
         }
         case 2:
         {
-            fstream arq("arquivo.txt", ios::in);
-            if (!arq.is_open())
-            {
-                cout << "Erro ao abrir o arquivo!" << endl;
-                return 1;
-            }
-            cout << "\n--------- PESSOAS CADASTRADAS ---------" << endl;
-            while (getline(arq, nome))
-            {
-                getline(arq, altura_str);
-                getline(arq, idade_str);
-                cout << "Nome: " << nome << endl;
-                cout << "Altura: " << altura_str << endl;
-                cout << "Idade: " << idade_str << endl;
-                cout << "---------------------------------------" << endl;
-            }
-
-            arq.close();
+            listaPessoas();
             break;
         }
         default:
