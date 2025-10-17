7139452e93eabd96ba2d0e0aadaa2140feaa1d73
Valdir de Souza Carvalho Neto <valdirneto100c@gmail.com>
2025-10-17 13:22:56 -0300

    Organizando LPR1

--- LPR 1 2025/Java/EquacaoSegundoMain.java
+++ LPR 1 2025/Java/EquacaoSegundoMain.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main1 {
+public class EquacaoSegundoMain {
     public static void main(String[] args) {
         Scanner cin = new Scanner(System.in);
         EquacaoSegundoGrau eq = new EquacaoSegundoGrau();
