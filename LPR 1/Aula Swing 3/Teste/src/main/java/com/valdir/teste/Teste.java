3252942c8babc9d010d458625824e26ea3bb1ab3
Valdir de Souza Carvalho Neto <0082934@IFMG.EDU.BR>
2026-01-14 10:23:49 -0300

    Aula Swing 3

---
+++ LPR 1/Aula Swing 3/Teste/src/main/java/com/valdir/teste/Teste.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.valdir.teste;
+
+import forms.Main;
+
+/**
+ *
+ * @author Valdir de Souza Carvalho Neto
+ */
+public class Teste {
+
+    public static void main(String[] args) {
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Main().setVisible(true);
+            }
+        });
+    }
+}
