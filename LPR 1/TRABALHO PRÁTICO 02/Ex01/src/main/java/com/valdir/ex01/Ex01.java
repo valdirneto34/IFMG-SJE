b332d684a60d1014b43c6935a30992f3f1d0663b
Valdir de Souza Carvalho Neto <valdirneto100c@gmail.com>
2026-01-14 23:11:16 -0300

    Update

---
+++ LPR 1/TRABALHO PRÁTICO 02/Ex01/src/main/java/com/valdir/ex01/Ex01.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.valdir.ex01;
+
+import forms.Main;
+
+/**
+ *
+ * @author valdi
+ */
+public class Ex01 {
+
+    public static void main(String[] args) {
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(() -> new Main().setVisible(true));
+    }
+}
---
+++ LPR 1/TRABALHO PRÁTICO 02/Ex02/src/main/java/com/valdir/ex02/Ex02.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.valdir.ex02;
+
+import forms.Main;
+
+/**
+ *
+ * @author valdi
+ */
+public class Ex02 {
+
+    public static void main(String[] args) {
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(() -> new Main().setVisible(true));
+    }
+}
